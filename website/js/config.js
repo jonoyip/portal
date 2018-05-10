153241afafa35b4dfa82e688fe757fcf459b2ff2
Jonathan Yip <39057467+jonoyip@users.noreply.github.com>
2018-05-10 15:40:25 +0800

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,13 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_IRw4g9Wrq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '107sjdu70fqpki8mda20clu4ka', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_3RKRldHAd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'tce1fuephg3uo5l4eah3p67fh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://3ma8vjromk.execute-api.ap-southeast-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'earl@eeg3.net'
+        email: 'jonathan@deskvault.com'
     }
 };
